10b9fb17e3098d0b8fa9dda6732f7539b7cbeb94
LanAnhPham12 <21130275@st.hcmuaf.edu.vn>
2024-06-27 16:10:49 +0700

    fix: Login, ProductList, data

--- src/pages/ProductList/Product/Product.jsx
+++ src/pages/ProductList/Product/Product.jsx
@@ -1,13 +1,14 @@
 import RatingStars from "./RatingStar/RatingStar";
+import styles from "../ProductList.module.css"
 function Product() {
     // const {dataProduct} = props
     return ( 
-        <div className="card-product">
+        <div className={`${styles['card-product']}`}>
             <div className="card h-100 w-80">
             <img src= "https://www.gardenparadiseseeds.com/cdn/shop/files/Garden_Order_Online_Shop_Luo_Han_Guo_Monk_fruit_Seeds_Siraitiagrosvenorii_258x396.png?v=1691401674" className="card-img-top" alt="..." sizes="258px"/>
                 <div className="card-body">
                     <div className="card-title text-truncate f-s-20">Monk fruit Seeds | Luo Han Guo - Siraitia grosvenorii</div>
-                    <div className="d-flex gap-2 justify-content-center align-items-center text-green">
+                    <div className={`d-flex gap-2 justify-content-center align-items-center ${styles['text-green']}`}>
                     <RatingStars score={4.5} />
                     10 reviews
                     </div>
